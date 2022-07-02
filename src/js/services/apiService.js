fce6afe6f2a2184fadadfad7a6e49a6b190578cb
kleonova <kleonova0@gmail.com>
2022-07-03 00:24:15 +0800

    search

--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 import config from '../config/apiConfig';
 
-/**
- * /countries - array of countries
- * /cities - array of cities
- * /prices/cheap - array
- */
 class Api {
   constructor(config) {
     this.url = config.url;
@@ -22,6 +17,15 @@ class Api {
   async cities() {
     try {
       const response = await axios.get(`${this.url}/cities`);
+      return response.data;
+    } catch (err) {
+      console.log(err);
+      return Promise.reject(err);
+    }
+  }
+  async airlines() {
+    try {
+      const response = await axios.get(`${this.url}/airlines`);
       return response.data;
     } catch (err) {
       console.log(err);
